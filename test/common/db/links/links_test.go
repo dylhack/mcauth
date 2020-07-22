dd8c385fdb00406f35e211b9cee1b4b1603f768c
Dylan Hackworth <dhpf@pm.me>
2020-07-22 18:23:28 -0500

    Added links_test

---
+++ test/common/db/links/links_test.go
@@ -0,0 +1,98 @@
+package links
+
+import (
+	"github.com/dhghf/mcauth/internal/common/db"
+	"os"
+	"testing"
+)
+
+var (
+	playerUUID = "5bce3068e4f3489fb66b5723b2a7cdb1"
+	discordID  = "164837347156951040"
+	store      *db.LinksTable
+)
+
+func TestMain(m *testing.M) {
+	if store == nil {
+		storeDB := db.GetStore("./mcauth-test.db")
+		store = &storeDB.Links
+	}
+	m.Run()
+}
+
+func TestLinks(t *testing.T) {
+	t.Run("NewLink", testNewLink)
+	t.Run("SetLink", testSetLink)
+	t.Run("GetDiscordID", testGetDiscordID)
+	t.Run("GetPlayerUUID", testGetPlayerUUID)
+	t.Run("GetAllLinks", testGetAllLinks)
+	t.Run("Unlink", func(t *testing.T) {
+		t.Run("UnLinkPlayerUUID", testUnLinkPlayerUUID)
+		t.Run("Re-link", testNewLink)
+		t.Run("UnLinkDiscordID", testUnLinkDiscordID)
+	})
+	t.Cleanup(func() { os.Remove("./mcauth-test.db") })
+}
+
+func testNewLink(t *testing.T) {
+	err := store.NewLink(discordID, playerUUID)
+
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func testSetLink(t *testing.T) {
+	err := store.SetLink(discordID, playerUUID)
+
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func testGetPlayerUUID(t *testing.T) {
+	result := store.GetPlayerID(discordID)
+
+	if playerUUID != result {
+		t.Errorf("GetPlayerID failed because \"%s\" != \"%s\"\n", playerUUID, result)
+	}
+}
+
+func testGetDiscordID(t *testing.T) {
+	result := store.GetDiscordID(playerUUID)
+
+	if discordID != result {
+		t.Errorf("GetDiscordID failed because \"%s\" != \"%s\"\n", discordID, result)
+	}
+}
+
+func testGetAllLinks(t *testing.T) {
+	result := store.GetAllLinks()
+
+	if len(result) == 0 {
+		t.Error("GetAllLinks returned nothing")
+	}
+
+	for _, accLink := range result {
+		if accLink.PlayerID == playerUUID && accLink.DiscordID == discordID {
+			return
+		}
+	}
+	t.Errorf("Could find link for (%s/%s)\n", playerUUID, discordID)
+}
+
+func testUnLinkDiscordID(t *testing.T) {
+	err := store.UnLink(discordID)
+
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func testUnLinkPlayerUUID(t *testing.T) {
+	err := store.UnLink(playerUUID)
+
+	if err != nil {
+		t.Error(err)
+	}
+}
