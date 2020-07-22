a47a18ce1cf6f88bcfac2a1171bf22e6f837d068
Dylan Hackworth <dhpf@pm.me>
2020-07-22 15:54:26 -0500

    Added alt verification

--- internal/bot/verifying.go
+++ internal/bot/verifying.go
@@ -1,19 +1,30 @@
 package bot
 
 import (
-	"fmt"
 	c "github.com/dhghf/mcauth/internal/common"
 	"log"
 )
 
 // if reason is empty and isValid is true then ignore reason
 func (bot *Bot) VerifyPlayer(playerID string) (bool, string) {
+	alt := bot.store.Alts.GetAlt(playerID)
+
+	// check if they're an alt account
+	if len(alt.Owner) > 0 {
+		userID := bot.store.Links.GetDiscordID(playerID)
+		return bot.VerifyDiscordUser(userID)
+	}
+
 	userID := bot.store.Links.GetDiscordID(playerID)
 
 	if len(userID) == 0 {
 		return false, c.NoLink
 	}
 
+	return bot.VerifyDiscordUser(userID)
+}
+
+func (bot *Bot) VerifyDiscordUser(userID string) (bool, string) {
 	member, err := bot.client.GuildMember(
 		bot.config.Guild,
 		userID,
@@ -30,33 +41,6 @@ func (bot *Bot) VerifyPlayer(playerID string) (bool, string) {
 	// if they're an admin then they pass all exceptions
 	if hasAdmin {
 		return true, "Administrator"
-	}
-
-	// Check if they're an alt account
-	altAcc := bot.store.Alts.GetAlt(playerID)
-
-	if len(altAcc.Owner) > 0 {
-		discordID := bot.store.Links.GetDiscordID(altAcc.Owner)
-
-		if len(discordID) > 0 {
-			member, err := bot.client.GuildMember(
-				bot.config.Guild,
-				discordID,
-			)
-
-			if err == nil {
-				isWhitelisted, _ = bot.CheckRoles(member.Roles)
-
-				// check if the owner of the alt account is whitelisted
-				if isWhitelisted {
-					return true, fmt.Sprintf(
-						"Alt account of %s#%s",
-						member.User.Username,
-						member.User.Discriminator,
-					)
-				}
-			}
-		}
 	}
 
 	// if they're a regular user then check if they have the right roles
